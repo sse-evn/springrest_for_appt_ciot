16ee1f508a73b291cec1bd8b145d2b76776917d2
evn <collegemab12@gmail.com>
2025-02-28 17:17:50 +0500

    fix bugs ;(

--- src/main/java/com/evn/websocket/WebSocketConfig.java
+++ src/main/java/com/evn/websocket/WebSocketConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandler(), "/ws/users").setAllowedOrigins("*");
+        registry.addHandler(new WebSocketHandler(), "/ws/users")
+                .setAllowedOrigins("*"); // Разрешить CORS (можно ограничить)
     }
 }
